25f68e659d5c90f36979f6934e78cd64b336a493
supergaming9999 <superdagaming@gmail.com>
2021-01-11 09:30:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ loadCommands(client);
 client.on('message', async (message) => {
     if (message.author.bot) return;
 
-	const messageArray = message.content.split(' ');
-	const cmd = messageArray[0];
+    const messageArray = message.content.split(' ');
+    const cmd = messageArray[0];
     const args = messageArray.slice(1);
     
-    const prefix = config.token;
+    const prefix = config.prefix;
 
-	if (!message.content.startsWith(prefix)) return;
+    if (!message.content.startsWith(prefix)) return;
     const commandfile = client.commands.get(cmd.slice(prefix.length)) || client.commands.get(client.aliases.get(cmd.slice(prefix.length)));
     commandfile.run(client, message, args);
 })
