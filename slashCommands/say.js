d63027081b9fdd86064e619ab413c64e691b2be1
BuildTools <unconfigured@null.spigotmc.org>
2023-10-29 13:24:11 -0400

    Fixed weird formatting

--- slashCommands/say.js
+++ slashCommands/say.js
@@ -4,23 +4,23 @@ module.exports = {
 	name: 'say',
 	description: "Make me say something.",
 	type: ApplicationCommandType.ChatInput,
-    default_member_permissions: "ManageMessages",
-    options: [
-        {
-            name: "text",
-            description: "What do you want me to say?",
-            type: ApplicationCommandOptionType.String,
-            required: true
-        }
-    ],
-    /**
-     * @param {Client} client 
-     * @param {ChatInputCommandInteraction} interaction 
-     * @param {Object} config 
-     */
+	default_member_permissions: "ManageMessages",
+	options: [
+		{
+			name: "text",
+			description: "What do you want me to say?",
+			type: ApplicationCommandOptionType.String,
+			required: true
+		}
+	],
+	/**
+	 * @param {Client} client 
+	 * @param {ChatInputCommandInteraction} interaction 
+	 * @param {Object} config 
+	 */
 	run: async (client, interaction, config) => {
-        var text = interaction.options.getString("text");
-        await interaction.channel.send({ content: text });
-        interaction.reply({ content: `Successfully said \`${text}\``, ephemeral: true });
+		var text = interaction.options.getString("text");
+		await interaction.channel.send({ content: text });
+		interaction.reply({ content: `Successfully said \`${text}\``, ephemeral: true });
 	}
 };
